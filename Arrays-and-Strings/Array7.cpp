09c4fc7c00c6dfa1a1e7082e56d4f2b6e163e039
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-03-22 18:32:22 +0530

    Update and rename Array7 to Array7.cpp
    Q7: RotateMatrix.cpp

--- Arrays-and-Strings/Array7.cpp
+++ Arrays-and-Strings/Array7.cpp
@@ -1,8 +1,8 @@
 /*1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. 
 Can you do this in place? */
 /*[1 2 3	   	[7 4 1
-   4 5 6   -->	8 5 2
-   7 8 9]	    9 6 3]
+   4 5 6   -->		8 5 2
+   7 8 9]	    	9 6 3]
    
   #48 Rotate Image*/
    
