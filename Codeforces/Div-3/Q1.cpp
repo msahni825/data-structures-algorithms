9f2f6335e4491c3a5eae040b0c993f488735831c
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-04-11 10:38:07 +0530

    Q1: Spy Detected

--- Codeforces/Div-3/Q1.cpp
+++ Codeforces/Div-3/Q1.cpp
@@ -1,1 +1,42 @@
+/*A: spy detected*/
+#include<bits/stdc++.h>
+using namespace std;
 
+int detectOddIndex(int arr[],int len){
+	int ans;
+	if(arr[0]!=arr[len-1]){
+		if(arr[len-1]!=arr[len-2]){
+			ans=len;
+			cout<<ans<<"\n";
+			return ans;
+		}
+		else{
+			ans=1;
+		}
+	}
+	for(int i=0;i<len;i++){
+		if(arr[i]!=arr[i+1] && arr[i]!=arr[len-1]){
+			ans=i+1;
+			cout<<ans;
+			cout<<"\n";
+			break;
+		}
+	}	
+	return ans;
+}
+int main(){
+	int arr[100];
+	int t;
+	//cout<<"enter t: "<<"\n";
+	cin>>t;
+	int len;
+	while(t--){
+		//cout<<"enter length: "<<"\n";
+		cin>>len;
+		for(int i=0;i<len;i++){
+			cin>>arr[i];
+		}
+		detectOddIndex(arr,len);
+	}
+	return 0;
+}
