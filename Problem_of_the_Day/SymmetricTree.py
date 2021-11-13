0fd941129d0e5ea53d97048879c584a1c8813d09
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-11-13 19:41:10 +0530

    Create SymmetricTree.py

---
+++ Problem_of_the_Day/SymmetricTree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSame(self,root1,root2):
+        if root1==None and root2==None:
+            return True
+        if root1==None or root2==None:
+            return False
+        return root1.val==root2.val and self.isSame(root1.left,root2.right) and self.isSame(root1.right,root2.left)
+    
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isSame(root,root)
