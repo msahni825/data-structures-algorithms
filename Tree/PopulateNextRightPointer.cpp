a35bbca69fd6a3afa61c5b23ccfd5d189492d746
Megha Sahni <42293547+msahni825@users.noreply.github.com>
2021-12-29 22:24:44 +0530

    Populating Next Right Pointers in Each Node

---
+++ Tree/PopulateNextRightPointer.cpp
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if(root){
+        dfs(root);
+    }
+    return root;
+};
+function dfs(node){
+    if(node.left){
+       node.left.next=node.right;
+       if(node.next){
+           node.right.next=node.next.left;
+       }
+        dfs(node.left);
+        dfs(node.right);
+    }
+}
